7b51b806e14502077bca97c27e39676eb4b1eda7
Raymond Oung <r.oung@hapi-robo.com>
2021-04-09 17:58:25 +0900

    Refactoring

--- dancebots/core/frame.py
+++ dancebots/core/frame.py
@@ -10,21 +10,25 @@ class Frame:
     _ONE = 0.7  # one bit duration [msec]
     _ZERO = 0.2  # zero bit duration [msec]
 
+    # @TODO Set empty frame, like list()
     def __init__(self, motor_l=[0] * 8, motor_r=[0] * 8, leds=[0] * 8, sample_rate=44100):
         # Non-static variables
         self._sample_rate = sample_rate
-        self._last_value = 1  # WAV value; toggles between -1 and +1
+        self._last_value = 1 # WAV value; toggles between +1 and -1
         self._bits = [] # list of bits
         self._frames = [] # list of frames
 
         if len(motor_l) != 8:
-            raise ValueError("Left motor must contain at least 8 values")
+            raise ValueError("Left motor must contain 8 values")
 
         if len(motor_r) != 8:
-            raise ValueError("Right motor must contain at least 8 values")
+            raise ValueError("Right motor must contain 8 values")
 
         if len(leds) != 8:
-            raise ValueError("LEDs must contain at least 8 values")
+            raise ValueError("LEDs must contain 8 values")
+
+        if sample_rate < 1 or not isinstance(sample_rate, int):
+            raise ValueError("sample_rate must be a positive integer")
 
         self._append(self._START)
 
@@ -40,28 +44,28 @@ class Frame:
 
     def _append(self, duration):
         self._last_value *= -1  # toggle value
-        num_samples = int(duration * (self._sample_rate / 1000.0))
+        num_samples = int(self._sample_rate * duration / 1000.0)
         for x in range(num_samples):
             self._bits.append(self._last_value)
 
     @property
     def bits(self):
+        # list of all bits
         return self._bits
 
     @property
     def frames(self):
+        # list of all frames
         return self._frames
 
     @property
-    def length(self):
-        return len(self._bits)
-
-    @property
     def duration(self):
-        sample_period = 1.0 / self._sample_rate
-        return len(self._bits) * sample_period  # [seconds]
+        # total duration of frame(s)
+        bit_period = 1.0 / self._sample_rate # [seconds]
+        return len(self._bits) * bit_period # [seconds]
 
     def __str__(self):
+        # pretty print frames
         lines = []
         for frame in self._frames:
             lines.append(str(frame))
@@ -75,15 +79,15 @@ class Frame:
         frame = deepcopy(self)
 
         # Append other instance
-        frame._frames += other._frames
+        frame._frames = frame._frames + other._frames
 
         # If last bit of this frame is the same as the first 
         # bit of the other frame, then flip the bits in the other frame
         # Note: First bit of a frame is always -1
         if frame._last_value == -1:
-            frame._bits = self.bits + [x*-1 for x  in other.bits]
+            frame._bits = self._bits + [x*-1 for x in other._bits]
         else:
-            frame._bits = self.bits + other.bits
+            frame._bits = self._bits + other._bits
 
         frame._last_value = other._last_value
         
