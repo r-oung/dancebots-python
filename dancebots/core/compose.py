1fcaf70fcaf0e8443821de3c11fdf041a7cfc930
R. Oung <44389573+r-oung@users.noreply.github.com>
2022-02-03 11:45:42 +0900

    Adds license to package files

--- dancebots/core/compose.py
+++ dancebots/core/compose.py
@@ -1,7 +1,20 @@
 # -*- coding: utf-8 -*-
-"""Compose class.
+"""Compose class
 
-A class for converting a list of moves and/or lights into steps.
+Copyright (C) 2021-2022 Raymond Oung
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 from .step import Step
 
--- dancebots/utils/plot.py
+++ dancebots/utils/plot.py
@@ -1,6 +1,22 @@
 # -*- coding: utf-8 -*-
-"""Plot module.
+"""Plot module
 
+Functions for displaying audio waveform plots.
+
+Copyright (C) 2021-2022 Raymond Oung
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 import matplotlib.pyplot as plt
 from ..core import Frame, Bitstream
